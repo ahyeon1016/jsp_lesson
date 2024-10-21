d370991c9ec8bd5f8fce2ba53cfb6db05872bd77
GangHyeon An <cccloud1016@gmail.com>
2024-10-21 15:57:26 +0900

    2024.10.17 [create] [exam11_4, exam11_5, exam11_6] [Exception - web.xml, try-catch]

---
+++ JSPBook/src/main/java/chapter11/exam11_5.java
@@ -0,0 +1,36 @@
+package chapter11;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/11_5")
+public class exam11_5 extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("get 도착");
+		
+		RequestDispatcher ds = req.getRequestDispatcher("chapter11/exam11_5.jsp");
+		ds.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("post도착");
+		
+		String num1 = req.getParameter("num1");
+		String num2 = req.getParameter("num2");
+		
+		int a = Integer.parseInt(num1);
+		int b = Integer.parseInt(num2);
+		
+		int c = a/b; //
+	}
+
+}
