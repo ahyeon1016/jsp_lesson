7e930c368218581b9cd5623effa781589f66ffe7
GangHyeon An <cccloud1016@gmail.com>
2024-10-24 17:14:01 +0900

    [create] [login_crud] [MySQL-connect]

---
+++ login_crud/src/main/java/controller/member_controller.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.IOException;
+
+import dao.member_repository;
+import dto.member_dto;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/join")
+public class member_controller extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher ds = req.getRequestDispatcher("join.jsp");
+		ds.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 전처리
+		req.setCharacterEncoding("utf-8");
+		
+		String id = req.getParameter("id");
+		String pwd = req.getParameter("pwd");
+		int age = Integer.parseInt(req.getParameter("age"));
+		
+		member_dto dto = new member_dto();
+		dto.setId(id);
+		dto.setPwd(pwd);
+		dto.setAge(age);
+		
+		// 모델 이동
+		member_repository mr = member_repository.getInstance();
+		mr.member_create(dto);
+	}
+
+}
---
+++ login_crud/src/main/java/dto/member_dto.java
@@ -0,0 +1,26 @@
+package dto;
+
+public class member_dto {
+	private String id;
+	private String pwd;
+	private int age;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
